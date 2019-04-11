6d011046bde65ff8bffbbe0dd79527c4b73a0f7f
Kiarmor <kiarmor2017@gmail.com>
2019-04-11 19:25:56 +0300

    commit before practice

---
+++ practice/functions_forms_tasks/1.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *Создать форму с двумя элементами textarea. При отправке формы скрипт
+должен выдавать только те слова, которые есть и в первом и во втором поле ввода.
+Реализацию это логики необходимо поместить в функцию getCommonWords($a, $b), которая
+будет возвращать массив с общими словами.
+ */
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Task 1</h1>
+
+<form action="">
+    <textare></textare>
+</form>
+
+</body>
+</html>
