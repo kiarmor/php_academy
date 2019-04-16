9f7b3d1ad8cbf8fe139bd20e61e12b2858e7ec29
Kiarmor <kiarmor2017@gmail.com>
2019-04-16 14:20:02 +0300

    task 2 done

---
+++ practice/functions_forms_tasks/2.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Создать форму с элементом textarea.
+ * При отправке формы скрипт должен выдавать ТОП3 длинных слов в тексте.
+ * Реализовать с помощью функции.</p>
+*/
+declare(strict_types=1);
+
+function getString ()
+{
+    $default = ' ';
+    $message = isset($_POST['message']) ? $_POST['message'] : $default;
+    return $message;
+}
+function separate ($val)
+{
+    $sep = ' ';
+    $arr = explode($sep, $val);
+    return $arr;
+}
+
+$mess = getString();
+
+$arr = separate($mess);
+
+usort($arr, function($a, $b){
+    return strlen($b) - strlen($a);
+});
+if ($mess!== ' ') {
+
+    for ($i = 0; $i < 3; $i++) {
+        echo $arr[$i] . "<br>";
+    }
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tasks</title>
+</head>
+<body>
+    <h1>Task 2</h1>
+    <form method="post">
+        <textarea name="message" cols="30" rows="10"></textarea> <br>
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
+
---
+++ practice/functions_forms_tasks/3.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Security
+ * Date: 16.04.2019
+ * Time: 10:42
+ */
